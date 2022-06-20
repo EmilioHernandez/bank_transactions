d048fec7fdeaf75e16088744fe79d7c06c47e7b9
Emilio H <emilio25informatic@gmail.com>
2022-06-20 13:27:35 -0400

    Creando mail y reactor para sugerencia de prestamo

--- app/Domain/Account/Reactors/OfferLoanReactor.php
+++ app/Domain/Account/Reactors/OfferLoanReactor.php
@@ -2,18 +2,23 @@
 
 namespace App\Domain\Account\Reactors;
 
-use App\Models\Account;
 use App\Domain\Account\Events\MoreMoneyNeeded;
 use App\Mail\LoanProposalMail;
+use App\Models\Account;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Mail;
 use Spatie\EventSourcing\EventHandlers\Reactors\Reactor;
 
+
 class OfferLoanReactor extends Reactor implements ShouldQueue
 {
+    /**
+     * @param MoreMoneyNeeded $event
+     */
     public function __invoke(MoreMoneyNeeded $event)
     {
-        $account = Account::where('uuid', $event->aggregateRootUuid())->first();
+        $account = Account::where('uuid', $event->aggregateRootUuid())
+            ->first();
 
         Mail::to($account->user)->send(new LoanProposalMail());
     }
