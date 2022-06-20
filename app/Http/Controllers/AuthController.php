89d3f2f6b8c2db239287be2888105ee5738136c9
Emilio H <emilio25informatic@gmail.com>
2022-06-20 11:39:47 -0400

    Archivos necesarios para autenticacion y gestion de cuentas

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Constants\HttpRequest;
+use App\Constants\Messages;
+use App\Http\Requests\AuthRequest;
+use GuzzleHttp\Promise\PromiseInterface;
+use Illuminate\Http\Client\Response;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Http;
+use Laravel\Passport\ClientRepository;
+
+/**
+ *
+ */
+class AuthController extends Controller
+{
+    /**
+     *
+     */
+    const GRANT_CLIENT = 2;
+
+    /**
+     *
+     */
+    const GRANT_TYPE = 'password';
+
+    /**
+     * @param ClientRepository $clients
+     */
+    public function __construct(
+        private ClientRepository $clients,
+    ) {}
+
+    /**
+     * Generate access token or error if credentials are incorrect.
+     *
+     * @param AuthRequest $request
+     * @return array|JsonResponse|mixed
+     */
+    public function login(AuthRequest $request)
+    {
+        $username = $request->get('username');
+        $password = $request->get('password');
+
+        $tokenResponse = $this->getToken($username, $password);
+
+        if ($tokenResponse->failed()) {
+            return response()->json(
+                ['message' => Messages::WRONG_CREDENTIALS],
+                HttpRequest::UNAUTHORIZED_CODE
+            );
+        }
+
+        return $tokenResponse->json();
+    }
+
+    /**
+     * Determine if the user's credentials are correct to return an access token or an error if applicable.
+     *
+     * @param $username
+     * @param $password
+     * @return PromiseInterface|Response|JsonResponse
+     */
+    public function getToken($username, $password)
+    {
+        $passportClient = $this->clients->find(self::GRANT_CLIENT);
+
+        if (!isset($passportClient->id)) {
+            return response()->json([
+                'message' => Messages::PASSPORT_CLIENT_NOT_EXIST,
+            ]);
+        }
+
+        return Http::asForm()->post(url('/oauth/token'), [
+            'grant_type' => self::GRANT_TYPE,
+            'client_id' => $passportClient->id,
+            'client_secret' => $passportClient->secret,
+            'username' => $username,
+            'password' => $password,
+            'scope' => '',
+        ]);
+    }
+}
---
+++ app/Http/Requests/AuthRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Constants\Messages;
+use Illuminate\Foundation\Http\FormRequest;
+
+class AuthRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            'username' => 'required',
+            'password' => 'required',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages(): array
+    {
+        return [
+            'username.required' => Messages::USERNAME_REQUIRED,
+            'password.required' => Messages::PASSWORD_REQUIRED
+        ];
+    }
+}
